855cb78198e50a7707e35c5394ebb50190a09d5b
Marcos Kuchak <mkuchak@hotmail.com>
2022-04-07 12:01:36 -0300

    build: add esbuild polyfill to work properly with cfw

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,4 +1,13 @@
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import NodeModulesPolyfill from '@esbuild-plugins/node-modules-polyfill'
 import { build } from 'esbuild'
+import alias from 'esbuild-plugin-alias'
+import NodeModule from 'module'
+
+const { NodeModulesPolyfillPlugin } = NodeModulesPolyfill
+
+const { createRequire } = NodeModule
+const require = createRequire(import.meta.url)
 
 try {
   await build({
@@ -10,6 +19,16 @@ try {
     entryPoints: ['./src/index.ts'],
     outdir: './dist',
     outExtension: { '.js': '.mjs' },
+    plugins: [
+      NodeGlobalsPolyfillPlugin({
+        process: true,
+        buffer: true,
+      }),
+      NodeModulesPolyfillPlugin(),
+      alias({
+        '@prisma/client': require.resolve('@prisma/client'),
+      }),
+    ],
   })
 } catch {
   process.exitCode = 1
