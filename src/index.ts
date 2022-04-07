2438d481ba3d9f5ade4ceb3965576b9ac3e8aaa6
Marcos Kuchak <mkuchak@hotmail.com>
2022-04-07 10:56:34 -0300

    build: add esbuild, wrangler and jest

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello, world!')
+function handleRequest (request: Request) {
+  console.log('Request Path: ', new URL(request.url).pathname)
+
+  return new Response('Hello, world!')
+}
+
+addEventListener('fetch', (event: FetchEvent) => {
+  event.respondWith(handleRequest(event.request))
+})
