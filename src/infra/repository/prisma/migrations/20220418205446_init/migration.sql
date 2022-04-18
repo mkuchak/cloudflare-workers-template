426cb632e10f76ab63ed2914e298108ed942bb1a
Marcos Kuchak <mkuchak@hotmail.com>
2022-04-18 17:55:52 -0300

    chore: update schema

--- src/infra/repository/prisma/migrations/20220418205446_init/migration.sql
+++ src/infra/repository/prisma/migrations/20220418205446_init/migration.sql
@@ -14,17 +14,29 @@ CREATE TABLE `User` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `UserRefreshToken` (
+CREATE TABLE `UserToken` (
     `id` VARCHAR(191) NOT NULL,
     `userId` VARCHAR(191) NOT NULL,
     `token` VARCHAR(191) NOT NULL,
+    `code` VARCHAR(191) NULL,
     `userAgent` VARCHAR(191) NULL,
     `lastIp` VARCHAR(191) NULL,
-    `isEmailVerification` BOOLEAN NOT NULL DEFAULT false,
+    `asn` INTEGER NULL,
+    `asOrganization` VARCHAR(191) NULL,
+    `timezone` VARCHAR(191) NULL,
+    `continent` VARCHAR(191) NULL,
+    `country` VARCHAR(191) NULL,
+    `region` VARCHAR(191) NULL,
+    `regionCode` VARCHAR(191) NULL,
+    `city` VARCHAR(191) NULL,
+    `postalCode` VARCHAR(191) NULL,
+    `longitude` VARCHAR(191) NULL,
+    `latitude` VARCHAR(191) NULL,
+    `isEmailToken` BOOLEAN NOT NULL DEFAULT false,
     `expiresAt` DATETIME(3) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `UserRefreshToken_token_key`(`token`),
+    UNIQUE INDEX `UserToken_token_key`(`token`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
