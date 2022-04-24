2c44bbcb8c5eb39455fd37f4be12386a476beb01
Marcos Kuchak <mkuchak@hotmail.com>
2022-04-24 01:53:55 -0300

    chore: remove unused code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
-// The router below can be used as an alternative to split into multiple route files
-// Just import this router, delete the next lines and keep the "export default"
-// import { router } from './infra/http/router/index'
-
 import { RepositoryFactoryPrisma } from './infra/factory/RepositoryFactoryPrisma'
-import { Router } from './infra/http/Router'
-import { WorktopHttp } from './infra/http/WorktopHttp'
-// import { IttyRouterHttp } from './infra/http/IttyRouterHttp'
+import { Router } from './infra/http/router/Router'
+import { WorktopHttp } from './infra/http/workers/WorktopHttp'
+// import { IttyRouterHttp } from './infra/http/workers/IttyRouterHttp'
+// import { ExpressFlareHttp } from './infra/http/workers/ExpressFlareHttp'
 
 const router = new Router(
-  new WorktopHttp(), // new IttyRouterHttp(),
+  new WorktopHttp(), // worktop@next may be not ready-to-use in production yet
+  // new IttyRouterHttp(),
+  // new ExpressFlareHttp(),
+  // new ExpressHttp(), // TODO: only works on Node.js (npm run dev:node)
   new RepositoryFactoryPrisma(),
 )
 
-router.init()
+router.init('/api/v1')
 
 export default {
-  fetch: router.http.listen,
+  async fetch<Environment = unknown, Context = unknown> (
+    request: Request,
+    env: Environment,
+    context: Context,
+  ) {
+    return router.http.listen(request, env, context)
+  },
 }
