aa7febb38735876f2aa7e8b2fa869acaa115ecea
Marcos Kuchak <mkuchak@hotmail.com>
2022-04-29 17:33:05 -0300

    chore: add in-memory repositories to execute unit/integration tests

---
+++ src/infra/factory/RepositoryFactoryInMemory.ts
@@ -0,0 +1,14 @@
+import { RepositoryFactory } from '@/domain/factory/RepositoryFactory'
+
+import { UserRepositoryInMemory } from '../repository/inMemory/UserRepositoryInMemory'
+import { UserTokenRepositoryInMemory } from '../repository/inMemory/UserTokenRepositoryInMemory'
+
+export class RepositoryFactoryInMemory implements RepositoryFactory {
+  createUserRepository (): UserRepositoryInMemory {
+    return new UserRepositoryInMemory()
+  }
+
+  createUserTokenRepository (): UserTokenRepositoryInMemory {
+    return new UserTokenRepositoryInMemory()
+  }
+}
---
+++ src/infra/repository/inMemory/UserRepositoryInMemory.ts
@@ -0,0 +1,26 @@
+import { User } from '@/domain/entity/User'
+import { UserRepository } from '@/domain/repository/UserRepository'
+
+export class UserRepositoryInMemory implements UserRepository {
+  private static instance: UserRepositoryInMemory;
+
+  constructor (private user: User[] = []) {
+    if (!UserRepositoryInMemory.instance) {
+      UserRepositoryInMemory.instance = this
+    }
+
+    return UserRepositoryInMemory.instance
+  }
+
+  async save (user: User): Promise<void> {
+    this.user.push(user)
+  }
+
+  async findById (id: string): Promise<User> {
+    return this.user.find((user) => user.id === id)
+  }
+
+  async findByEmail (email: string): Promise<User> {
+    return this.user.find((user) => user.email === email)
+  }
+}
