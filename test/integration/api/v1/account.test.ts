9227253f5bccad96282980aba112b341bba7259b
Marcos Kuchak <mkuchak@hotmail.com>
2022-05-01 14:18:19 -0300

    test: add fail tests in account module api

--- test/integration/api/v1/account.test.ts
+++ test/integration/api/v1/account.test.ts
@@ -23,58 +23,211 @@ afterAll(async () => {
 })
 
 describe('/api/v1/account', () => {
-  it('POST /register', async () => {
-    const input = {
-      email: `john_doe_${uuid.generate().toLowerCase()}@gmail.com`,
-      password: '12345@Aa',
-    }
+  describe('POST /register', () => {
+    it('should create user', async () => {
+      const input = {
+        email: `john_doe_${uuid.generate().toLowerCase()}@gmail.com`,
+        password: '12345@Aa',
+      }
 
-    const { status, data } = await axiosAPIClient.post(
-      '/account/register',
-      input,
-    )
+      const { status, data } = await axiosAPIClient.post(
+        '/account/register',
+        input,
+      )
 
-    expect(status).toBe(201)
-    expect(data).toEqual(
-      expect.objectContaining({
-        id: expect.any(String),
-        email: input.email,
-      }),
-    )
+      expect(status).toBe(201)
+      expect(data).toEqual(
+        expect.objectContaining({
+          id: expect.any(String),
+          email: input.email,
+        }),
+      )
+    })
+
+    it('should return error when email is already registered', async () => {
+      const input = {
+        email: `john_doe_${uuid.generate().toLowerCase()}@gmail.com`,
+        password: '12345@Aa',
+      }
+      await axiosAPIClient.post('/account/register', input)
+      const { status, data } = await axiosAPIClient.post(
+        '/account/register',
+        input,
+      )
+      expect(status).toBe(409)
+      expect(data).toEqual(
+        expect.objectContaining({
+          error: 'User Already Exists',
+        }),
+      )
+    })
+
+    it('should return error when email is invalid', async () => {
+      const input = {
+        email: 'johndoe+1@gmail.com',
+        password: '12345@Aa',
+      }
+      const { status, data } = await axiosAPIClient.post(
+        '/account/register',
+        input,
+      )
+      expect(status).toBe(400)
+      expect(data).toEqual(
+        expect.objectContaining({
+          error: 'Invalid Email',
+        }),
+      )
+    })
+
+    it('should return error when password is invalid', async () => {
+      const input = {
+        email: `john_doe_${uuid.generate().toLowerCase()}@gmail.com`,
+        password: '12345',
+      }
+      const { status, data } = await axiosAPIClient.post(
+        '/account/register',
+        input,
+      )
+      expect(status).toBe(400)
+      expect(data).toEqual(
+        expect.objectContaining({
+          error: 'Weak Password',
+        }),
+      )
+    })
   })
 
-  it('POST /authenticate', async () => {
-    const input = {
-      email: `john_doe_${uuid.generate().toLowerCase()}@gmail.com`,
-      password: '12345@Aa',
-    }
-    await axiosAPIClient.post('/account/register', input)
+  describe('POST /authenticate', () => {
+    it('should authenticate user', async () => {
+      const input = {
+        email: `john_doe_${uuid.generate().toLowerCase()}@gmail.com`,
+        password: '12345@Aa',
+      }
+      await axiosAPIClient.post('/account/register', input)
 
-    const { status, data } = await axiosAPIClient.post(
-      '/account/authenticate',
-      input,
-    )
+      const { status, data } = await axiosAPIClient.post(
+        '/account/authenticate',
+        input,
+      )
 
-    expect(status).toBe(201)
-    expect(data).toHaveProperty('accessToken')
-    expect(data).toHaveProperty('refreshToken')
+      expect(status).toBe(201)
+      expect(data).toHaveProperty('accessToken')
+      expect(data).toHaveProperty('refreshToken')
+    })
+
+    it('should return error when email is invalid', async () => {
+      const input = {
+        email: 'johndoe+1@gmail.com',
+        password: '12345@Aa',
+      }
+      const { status, data } = await axiosAPIClient.post(
+        '/account/authenticate',
+        input,
+      )
+      expect(status).toBe(401)
+      expect(data).toEqual(
+        expect.objectContaining({
+          error: 'Invalid Password',
+        }),
+      )
+    })
+
+    it('should return error when password is invalid', async () => {
+      const input = {
+        email: `john_doe_${uuid.generate().toLowerCase()}@gmail.com`,
+        password: '12345',
+      }
+      const { status, data } = await axiosAPIClient.post(
+        '/account/authenticate',
+        input,
+      )
+      expect(status).toBe(401)
+      expect(data).toEqual(
+        expect.objectContaining({
+          error: 'Invalid Password',
+        }),
+      )
+    })
+
+    it('should return error when email is not registered', async () => {
+      const input = {
+        email: `john_doe_${uuid.generate().toLowerCase()}@gmail.com`,
+        password: '12345@Aa',
+      }
+      const { status, data } = await axiosAPIClient.post(
+        '/account/authenticate',
+        input,
+      )
+      expect(status).toBe(401)
+      expect(data).toEqual(
+        expect.objectContaining({
+          error: 'Invalid Password',
+        }),
+      )
+    })
+
+    it('should return error when password is wrong', async () => {
+      const input = {
+        email: `john_doe_${uuid.generate().toLowerCase()}@gmail.com`,
+        password: '12345@Aa',
+      }
+      await axiosAPIClient.post('/account/register', input)
+      const { status, data } = await axiosAPIClient.post(
+        '/account/authenticate',
+        {
+          email: input.email,
+          password: '123456',
+        },
+      )
+      expect(status).toBe(401)
+      expect(data).toEqual(
+        expect.objectContaining({
+          error: 'Invalid Password',
+        }),
+      )
+    })
   })
 
-  it('PUT /sessions/:refreshToken', async () => {
-    const input = {
-      email: `john_doe_${uuid.generate().toLowerCase()}@gmail.com`,
-      password: '12345@Aa',
-    }
-    await axiosAPIClient.post('/account/register', input)
+  describe('PUT /sessions/:refreshToken', () => {
+    it('should refresh access token', async () => {
+      const input = {
+        email: `john_doe_${uuid.generate().toLowerCase()}@gmail.com`,
+        password: '12345@Aa',
+      }
+      await axiosAPIClient.post('/account/register', input)
 
-    const { data } = await axiosAPIClient.post('/account/authenticate', input)
+      const { data } = await axiosAPIClient.post(
+        '/account/authenticate',
+        input,
+      )
 
-    const { status, data: newData } = await axiosAPIClient.put(
-      `/account/sessions/${data.refreshToken}`,
-    )
+      const { status, data: newData } = await axiosAPIClient.put(
+        `/account/sessions/${data.refreshToken}`,
+      )
 
-    expect(status).toBe(201)
-    expect(newData).toHaveProperty('accessToken')
-    expect(newData).toHaveProperty('refreshToken')
+      expect(status).toBe(201)
+      expect(newData).toHaveProperty('accessToken')
+      expect(newData).toHaveProperty('refreshToken')
+    })
+
+    it('should return error when refresh token is invalid', async () => {
+      const input = {
+        email: `john_doe_${uuid.generate().toLowerCase()}@gmail.com`,
+        password: '12345@Aa',
+      }
+      await axiosAPIClient.post('/account/register', input)
+      await axiosAPIClient.post('/account/authenticate', input)
+
+      const { status, data } = await axiosAPIClient.put(
+        '/account/sessions/wrong-refresh-token',
+      )
+
+      expect(status).toBe(401)
+      expect(data).toEqual(
+        expect.objectContaining({
+          error: 'Invalid Refresh Token',
+        }),
+      )
+    })
   })
 })
