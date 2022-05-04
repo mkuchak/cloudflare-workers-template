f12d466e6b0400951e5569881631fb90e19fd010
Marcos Kuchak <mkuchak@hotmail.com>
2022-05-04 17:08:58 -0300

    chore: create JWT adapter

---
+++ src/infra/adapter/jwt/CfwJWTAdapter.ts
@@ -0,0 +1,17 @@
+import jwt from '@tsndr/cloudflare-worker-jwt'
+
+import { JWT } from './JWT'
+
+export class CfwJWTAdapter implements JWT {
+  async sign (payload: any, secret: string, options?: any): Promise<string> {
+    return await jwt.sign(payload, secret, options)
+  }
+
+  async verify (token: string, secret: string, options?: any): Promise<boolean> {
+    return await jwt.verify(token, secret, options)
+  }
+
+  decode (token: string): any {
+    return jwt.decode(token)
+  }
+}
