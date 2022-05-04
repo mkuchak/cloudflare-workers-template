f17cba606032d69f4b3a043222481d88bd960cd1
Marcos Kuchak <mkuchak@hotmail.com>
2022-05-04 17:10:03 -0300

    chore: config extract authentication settings from environment

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
 export const config = {
   nodeEnv: process.env.NODE_ENV,
   dataProxyURL: process.env.DATAPROXY_URL,
+  jwtSecret: process.env.JWT_SECRET || 'secret',
+  jwtExpiration: Number(process.env.JWT_EXPIRATION) || 15, // minutes
+  refreshTokenExpiration: Number(process.env.REFRESH_TOKEN_EXPIRATION) || 30, // days
 }
